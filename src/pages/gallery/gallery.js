9cc3bc7229559420c699f68766e6b96db6eafebc
Rebeca <mustelierrebeca@gmail.com>
2024-03-14 21:56:49 -0500

    Programming the Navbar

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -5,30 +5,25 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Comentarios from "../../components/comentarios/comentarios";
 
-import Capture1  from "./imagenes/Capture1.PNG"
-import Capture2  from "./imagenes/Capture2.PNG"
-import Capture3  from "./imagenes/Capture3.PNG"
-import S1 from "./imagenes/S1.png"
-import S2 from "./imagenes/S2.png"
-import S3 from "./imagenes/S3.png"
-import S4 from "./imagenes/S4.png"
-import R2 from "./imagenes/R2.jpg"
 import I1 from "./imagenes/I1.jpg"
 import I2 from "./imagenes/I2.jpg"
 import I3 from "./imagenes/I3.jpg"
+import I4 from "./imagenes/I4.jpg"
+import I5 from "./imagenes/I5.jpg"
+import I6 from "./imagenes/I6.jpg"
 
 
 
-const IMAGES = [S1, S2, S3, S4, Capture1,Capture2, Capture3, R2, I1, I2, I3]
+const IMAGES = [I1, I2, I3, I4, I5, I6]
 
 export default function Gallery(){
     return(
         <div className={styles.contenedorPrincipal} >
       <div className={styles.contenedorCarrusel}>
-        <Carousel className={styles.carousel}>
+        <Carousel variant="dark" className={styles.carousel}>
           {IMAGES.map((image, index) => {
             return(
-              <Carousel.Item key={index} className={styles.carouselItem}>
+              <Carousel.Item interval={9000} key={index} className={styles.carouselItem}>
               <div className={styles.carouselItemDiv}>
                 <div className={styles.imageDiv}>
                   <img className={styles.imagen} src={image} />
