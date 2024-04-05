28a27a885185330629c645abeb0bd4e7a6323ab9
Rebeca <mustelierrebeca@gmail.com>
2024-04-04 21:21:10 -0500

    2k resolution view

--- src/pages/layout/layout.js
+++ src/pages/layout/layout.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Capture1 from "./imagenes/Capture1.PNG";
-import Capture from "./imagenes/Capture.PNG";
 import styles from "./layout.module.css";
-import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-import { CgMail } from "react-icons/cg";
-import { Link } from 'react-router-dom';
-import ContactUs from './email';
 import NavBar from '../../components/navbar/navbar';
 
 import Footer from '../../components/footer/footer';
 
 export default function Layout({ children }) {
     return (
-        <div className={styles.layout}>
-            <div>
+        <div className={styles.main__container}>
+            <div   className={styles.layout}>
                 <NavBar />
 
                 <div className={styles.body}>
